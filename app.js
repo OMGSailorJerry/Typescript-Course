438bcdaa96c7a781a66a27ac9649c494cfed9f60
Oleg Mikhailov <omikhailov@ace-st.com>
2018-02-27 22:47:22 +0200

    JS Libs

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log('test s 10');
+require("jQuery");
+$('#app').css({ 'background-color': 'green' });
 //# sourceMappingURL=app.js.map
